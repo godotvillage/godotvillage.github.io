6cc0c800ed30783eb7093ebe9d74ad9833138c42
moshangzhu <925236118@qq.com>
2026-05-07 17:03:14 +0800

    feat: 修改路由为hash模式，防止githubpage刷新后无法加载页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import { ElMessage } from 'element-plus'
 import { useAuthStore } from '@/stores/auth'
@@ -170,7 +170,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior() {
     return { top: 0 }
