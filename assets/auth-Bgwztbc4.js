8bee6ecbaff127aac22fc2328843bcb3cec2ab30
925236118 <925236118@qq.com>
2026-05-15 10:46:43 +0000

    Deploying to gh-pages from @ godotvillage/godotvillage.github.io@faf4b72a8976890ccfc1872b71494445bc9ef8ff 🚀

--- assets/auth-Bgwztbc4.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./request-Vx3nzaGn.js";const o={login(r){return t.post("/auth/login",r)},register(r){return t.post("/auth/register",r)}};export{o as a};
---
+++ assets/stats-B3ECJp8I.js
@@ -0,0 +1,1 @@
+import{r as t}from"./request-BpnxGO4H.js";const e={getSiteStats(){return t.get("/stats/site")},getUserStatistics(){return t.get("/stats/users")}};export{e as s};
