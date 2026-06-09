36557f7ffdb69cee041bc75af4e6abd49ccc9bcc
godotvillage <godotvillage@163.com>
2026-06-09 11:18:31 +0800

    deploy: update frontend 2026-06-09_1118

---
+++ assets/avatar-Dnemtaw_.js
@@ -0,0 +1,1 @@
+const i="https://www.godotvillage.com",r=(t,e)=>e?i+e:t?`https://api.dicebear.com/7.x/initials/svg?seed=${encodeURIComponent(t)}`:"https://api.dicebear.com/7.x/initials/svg?seed=User";export{r as g};
